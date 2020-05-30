d3892b7f007178c90762c45c0668bd30b13afb54
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2020-05-29 19:54:15 -0500

    Add new img style

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,21 +5,9 @@ var store = [{
         "url": "http://localhost:4000/general/post-modified/",
         "teaser": null
       },{
-        "title": "Post: Standard",
-        "excerpt":"All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she...","categories": ["docs","General"],
-        "tags": [],
-        "url": "http://localhost:4000/docs/general/post-modified/",
-        "teaser": null
-      },{
         "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["General"],
         "tags": [],
         "url": "http://localhost:4000/general/welcome-to-jekyll/",
-        "teaser": null
-      },{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["docs","General"],
-        "tags": [],
-        "url": "http://localhost:4000/docs/general/welcome-to-jekyll/",
         "teaser": null
       }]
