57013a624921f4fcbeb5383b7c32028d905c1868
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2020-05-29 19:04:44 -0500

    Add test

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Post: Standard",
+        "excerpt":"All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she...","categories": ["General"],
+        "tags": [],
+        "url": "http://localhost:4000/general/2020/05/28/post-modified.html",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["General"],
+        "tags": [],
+        "url": "http://localhost:4000/general/2020/05/29/welcome-to-jekyll.html",
+        "teaser": null
+      }]
